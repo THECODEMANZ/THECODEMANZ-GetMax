d22cf5f8fe9edaf3ebf7d664bc2f84bf923a32ac
THECODEMANZ <kewabailey@gmail.com>
2023-03-01 06:06:48 +0000

    Completed Lab Get Max Test

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -17,6 +17,7 @@ public class GetMax {
         for(int i = 0; i < arr.length; i++){
             
         
+            
             if (arr[i] >= 0) {
                 if(arr[i] > posMax){
                     posMax = arr[i];
